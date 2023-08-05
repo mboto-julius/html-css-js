3833f2c41a4bbfe547f8b2e4cdac8eda3c79234a
Julius Mboto <juliusmboto@gmail.com>
2023-08-05 11:34:29 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-var description = "We need a new floor";
-
-console.log(description);
+const description = "We need a new floor";
+const squareMeter = 100;
+const floorOption = ["carpet", "hood", "titles"];
+const renovationJob = {
+  ownerName: "John",
+  maximumPrice: 8000,
+  category: "bathroam",
+  newShower: true,
+};
+const error = {
+  statusCode: 404,
+  description: "Server Error",
+  retry: true,
+};
+// console.log(error.statusCode);
+// traditional functions vs arrow  functions
+function calculatePrice() {
+  const price = 1000 + 3000;
+  console.log(price);
+}
+// invoke or call the function
+calculatePrice();
