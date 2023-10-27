65ea75a4e157e4b7f4330901e4f4f6b4b7bdeb42
Julius Mboto <juliusmboto@gmail.com>
2023-10-27 12:20:42 +0300

    working with pig game

--- js/script.js
+++ js/script.js
@@ -1,30 +1,88 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModel = document.querySelector('.close-modal');
-const btnsOpenModel = document.querySelectorAll('.show-modal');
+// selecting element
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const currentOElement = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-const openModel = function(){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+let scores, currentScore, activePlayer, playing;
+
+// starting conditions
+const init = function (){
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0Element.textContent = 0;
+    score1Element.textContent = 0;
+    currentOElement.textContent = 0;
+    current1Element.textContent = 0;
+
+    diceElement.classList.add('hidden');
+    player0Element.classList.remove('player--winner');
+    player1Element.classList.remove('player--winner');
+    player0Element.classList.add('player--active');
+    player1Element.classList.remove('player--active');
 };
 
-for(let i=0; i<btnsOpenModel.length; i++ ){
-    btnsOpenModel[i].addEventListener('click', openModel);
-} 
+init();
 
-const closeModal = function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
 };
 
-btnCloseModel.addEventListener('click', closeModal); 
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function(e){
-    // console.log(e.key);
-    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
-        closeModal();
+// rolling dice functionality
+btnRoll.addEventListener('click', function(){
+    if(playing){
+        //  1. generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        
+        // 2. display dice
+        diceElement.classList.remove('hidden');
+        diceElement.src = `/images/dice-${dice}.png`;
+    
+        // 3. check for rolled 1
+        if(dice !== 1){
+        // add dice to currect score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        }else{
+            // switch to next player
+            switchPlayer();
+        } 
     }
 });
+
+btnHold.addEventListener('click', function(){
+    if(playing){
+        // 1. add current score to the active player's score
+        scores[activePlayer] += currentScore;
+        // scores[1] = scores[1] + currentScore;
+        // scores[0] = scores[0] + currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]; 
+
+        // 2. check if player's score is >= 100 (finish game)
+        if(scores[activePlayer] >= 100){
+            playing = false;
+            diceElement.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        }else{
+            switchPlayer();
+        }
+    }
+});
+
+btnNew.addEventListener('click', init);
